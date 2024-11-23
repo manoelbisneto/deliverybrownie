5d586382cb3a677df2cac274e622b453489c1bdf
Allan Marques <allanlima00@gmail.com>
2024-11-22 22:33:30 -0300

    Banco de dados 2.0 e pdf do projeto

--- delivery_db.sql
+++ delivery_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14/11/2024 às 23:53
+-- Tempo de geração: 23/11/2024 às 02:27
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -65,11 +65,9 @@ CREATE TABLE `itens_menu` (
 --
 
 INSERT INTO `itens_menu` (`id`, `categorias_id`, `nome`, `descricao`, `preco`, `disponivel`, `criado_em`, `atualizado_em`) VALUES
-(1, 1, '1', '1', 1.00, 1, '2024-11-14 20:36:37', '2024-11-14 20:36:37'),
-(2, 1, '2', '2', 2.00, 1, '2024-11-14 20:37:07', '2024-11-14 20:37:07'),
-(3, 1, '3', '3', 3.00, 1, '2024-11-14 20:37:54', '2024-11-14 20:37:54'),
-(4, 1, '4', '4', 4.00, 1, '2024-11-14 20:39:19', '2024-11-14 20:39:19'),
-(5, 3, '5', '5', 5.00, 0, '2024-11-14 20:41:53', '2024-11-14 20:41:53');
+(1, 3, 'brownie 11', '1', 1.00, 1, '2024-11-14 20:36:37', '2024-11-22 16:59:56'),
+(2, 1, 'brownie 2', '2', 2.00, 1, '2024-11-14 20:37:07', '2024-11-22 00:06:04'),
+(3, 1, '3', '3', 3.00, 1, '2024-11-14 20:37:54', '2024-11-14 20:37:54');
 
 -- --------------------------------------------------------
 
@@ -84,6 +82,39 @@ CREATE TABLE `itens_pedidos` (
   `quantidade` int(11) NOT NULL,
   `preco` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `itens_pedidos`
+--
+
+INSERT INTO `itens_pedidos` (`id`, `pedidos_id`, `itens_menu_id`, `quantidade`, `preco`) VALUES
+(1, 1, 1, 5, 1.00),
+(2, 1, 2, 4, 2.00),
+(3, 1, 3, 1, 3.00),
+(4, 2, 1, 1, 1.00),
+(5, 3, 1, 3, 1.00),
+(6, 4, 1, 2, 1.00),
+(7, 5, 2, 2, 2.00),
+(8, 5, 1, 3, 1.00),
+(9, 5, 3, 1, 3.00),
+(10, 6, 1, 8, 1.00),
+(11, 7, 3, 1, 3.00),
+(12, 7, 1, 2, 1.00),
+(13, 8, 3, 2, 3.00),
+(14, 9, 3, 1, 3.00),
+(15, 10, 3, 1, 3.00),
+(16, 11, 3, 1, 3.00),
+(17, 11, 1, 2, 1.00),
+(18, 12, 2, 2, 2.00),
+(19, 13, 1, 3, 1.00),
+(20, 14, 2, 1, 2.00),
+(21, 14, 1, 4, 1.00),
+(22, 15, 2, 3, 2.00),
+(23, 16, 1, 1, 1.00),
+(24, 16, 2, 1, 2.00),
+(25, 16, 3, 1, 3.00),
+(26, 17, 1, 1, 1.00),
+(27, 17, 2, 1, 2.00);
 
 -- --------------------------------------------------------
 
@@ -100,6 +131,29 @@ CREATE TABLE `pedidos` (
   `atualizado_em` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Despejando dados para a tabela `pedidos`
+--
+
+INSERT INTO `pedidos` (`id`, `usuarios_id`, `estado`, `preco_total`, `criado_em`, `atualizado_em`) VALUES
+(1, 1, 'cancelado', 16.00, '2024-11-22 18:16:41', '2024-11-22 18:34:08'),
+(2, 1, 'cancelado', 1.00, '2024-11-22 18:17:33', '2024-11-22 18:28:43'),
+(3, 1, 'cancelado', 3.00, '2024-11-22 18:17:59', '2024-11-22 18:33:41'),
+(4, 1, 'cancelado', 2.00, '2024-11-22 18:37:39', '2024-11-22 18:37:57'),
+(5, 1, 'cancelado', 10.00, '2024-11-22 18:38:38', '2024-11-22 18:38:47'),
+(6, 1, 'cancelado', 8.00, '2024-11-22 18:39:51', '2024-11-22 18:40:04'),
+(7, 1, 'cancelado', 5.00, '2024-11-22 18:40:30', '2024-11-22 18:40:37'),
+(8, 1, 'cancelado', 6.00, '2024-11-22 18:41:11', '2024-11-22 18:41:19'),
+(9, 1, 'cancelado', 3.00, '2024-11-22 18:42:21', '2024-11-22 18:42:31'),
+(10, 1, 'cancelado', 3.00, '2024-11-22 18:44:49', '2024-11-22 18:45:11'),
+(11, 1, 'cancelado', 5.00, '2024-11-22 18:45:49', '2024-11-22 18:46:00'),
+(12, 1, 'cancelado', 4.00, '2024-11-22 18:46:57', '2024-11-22 18:47:03'),
+(13, 1, 'cancelado', 3.00, '2024-11-22 18:47:39', '2024-11-22 19:02:26'),
+(14, 1, 'completo', 6.00, '2024-11-22 19:04:57', '2024-11-22 19:05:46'),
+(15, 1, 'cancelado', 6.00, '2024-11-22 19:06:21', '2024-11-22 19:06:34'),
+(16, 2, 'pendente', 6.00, '2024-11-22 19:10:07', '2024-11-22 19:10:07'),
+(17, 1, 'pendente', 3.00, '2024-11-22 20:37:33', '2024-11-22 20:37:33');
+
 -- --------------------------------------------------------
 
 --
@@ -111,6 +165,7 @@ CREATE TABLE `usuarios` (
   `nome` varchar(50) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `endereco` text NOT NULL,
   `tipo` enum('admin','usuario') DEFAULT 'usuario',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
@@ -120,9 +175,9 @@ CREATE TABLE `usuarios` (
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `senha`, `email`, `tipo`, `created_at`, `updated_at`) VALUES
-(1, 'adminTest', '$2y$10$S2bYTFo8GLVL6gUiJ39.o.owQNrLTJZ4mSJVDtjCHDQwT4vknsbr6', '111@111', 'admin', '2024-11-14 16:29:33', '2024-11-14 16:29:33'),
-(2, 'userTest', '$2y$10$093LvjJfT9wIAmMYI5zlbu4U7e/WQY.qvGJzjjOLWMFZAs8Kud0lG', '222@222', 'usuario', '2024-11-14 16:30:35', '2024-11-14 16:30:35');
+INSERT INTO `usuarios` (`id`, `nome`, `senha`, `email`, `endereco`, `tipo`, `created_at`, `updated_at`) VALUES
+(1, 'adminTest', '$2y$10$S2bYTFo8GLVL6gUiJ39.o.owQNrLTJZ4mSJVDtjCHDQwT4vknsbr6', '111@111', '', 'admin', '2024-11-14 16:29:33', '2024-11-14 16:29:33'),
+(2, 'userTest', '$2y$10$093LvjJfT9wIAmMYI5zlbu4U7e/WQY.qvGJzjjOLWMFZAs8Kud0lG', '222@222', '', 'usuario', '2024-11-14 16:30:35', '2024-11-14 16:30:35');
 
 --
 -- Índices para tabelas despejadas
@@ -184,13 +239,13 @@ ALTER TABLE `itens_menu`
 -- AUTO_INCREMENT de tabela `itens_pedidos`
 --
 ALTER TABLE `itens_pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de tabela `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
